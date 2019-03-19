1d4603f87475487d422e32031535f0e2b7da3d14
Dmitry Dushkin <dndushkin@yandex-team.ru>
2019-03-19 16:36:58 +0300

    Add ios < 11 support

--- ios/RNSafeAreaInsets/RNSafeAreaInsets.m
+++ ios/RNSafeAreaInsets/RNSafeAreaInsets.m
@@ -15,22 +15,33 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(currentInsets:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        UIViewController *currentController = RCTPresentedViewController();
-        UIView *rootView = currentController != nil ? currentController.view : nil;
-        UIEdgeInsets insets = UIEdgeInsetsZero;
-        if ( rootView != nil )
-        insets = rootView.safeAreaInsets;
+    if ([[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion:(NSOperatingSystemVersion){11, 0, 0}]) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UIViewController *currentController = RCTPresentedViewController();
+            UIView *rootView = currentController != nil ? currentController.view : nil;
+            UIEdgeInsets insets = UIEdgeInsetsZero;
+            if ( rootView != nil ) {
+                insets = rootView.safeAreaInsets;
+            }
 
+            NSDictionary *result = @{
+                                     @"left":@(insets.left),
+                                     @"right":@(insets.right),
+                                     @"top":@(insets.top),
+                                     @"bottom":@(insets.bottom)
+                                     };
+
+            resolve( result );
+        });
+    } else {
         NSDictionary *result = @{
-                                 @"left":@(insets.left),
-                                 @"right":@(insets.right),
-                                 @"top":@(insets.top),
-                                 @"bottom":@(insets.bottom)
+                                 @"left": @0,
+                                 @"right": @0,
+                                 @"top": @0,
+                                 @"bottom": @0
                                  };
-
-        resolve( result );
-    });
+        resolve(result);
+    }
 }
 
 @end
